acf11175ed1575068e870f8f9a07b3d4ee3c303e
Christopher Rhodes <chrhodes@cox.net>
2023-07-07 16:33:40 -0700

    Adding Reload Config to UI

---
+++ VNCLogViewer/Presentation/Views/UIConfig.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows;
+
+using VNCLogViewer.Presentation.ViewModels;
+
+using VNC;
+using VNC.Core.Mvvm;
+
+namespace VNCLogViewer.Presentation.Views
+{
+    public partial class UIConfig : ViewBase, IUIConfig, IInstanceCountV
+    {
+
+        #region Constructors and Load
+        
+        public UIConfig()
+        {
+            Int64 startTicks = Log.CONSTRUCTOR("Enter", Common.LOG_CATEGORY);
+
+            InstanceCountV++;
+            InitializeComponent();
+            
+			// Expose ViewModel
+						
+            // If View First with ViewModel in Xaml
+
+            // ViewModel = (IUIConfigViewModel)DataContext;
+
+            // Can create directly
+            // ViewModel = UIConfigViewModel();
+
+            Log.CONSTRUCTOR("Exit", Common.LOG_CATEGORY, startTicks);
+        }
+        
+        //public UIConfig(IUIConfigViewModel viewModel)
+        //{
+        //    Int64 startTicks = Log.CONSTRUCTOR($"Enter viewModel({viewModel.GetType()}", Common.LOG_CATEGORY);
+
+        //    InstanceCountV++;
+        //    InitializeComponent();
+
+        //    ViewModel = viewModel;
+
+        //    Log.CONSTRUCTOR("Exit", Common.LOG_CATEGORY, startTicks);
+        //}
+       
+        #endregion
+
+        #region Enums (None)
+
+
+        #endregion
+
+        #region Structures (None)
+
+
+        #endregion
+
+        #region Fields and Properties (None)
+
+
+        #endregion
+
+        #region Event Handlers (None)
+
+
+        #endregion
+
+        #region Commands (None)
+         
+        #endregion
+
+        #region Public Methods (None)
+
+
+        #endregion
+
+        #region Protected Methods (None)
+
+
+        #endregion
+
+        #region Private Methods (None)
+
+
+        #endregion   
+        
+        #region IInstanceCount
+
+        private static int _instanceCountV;
+
+        public int InstanceCountV
+        {
+            get => _instanceCountV;
+            set => _instanceCountV = value;
+        }
+
+        #endregion
+
+        private void btnReloadConfig_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

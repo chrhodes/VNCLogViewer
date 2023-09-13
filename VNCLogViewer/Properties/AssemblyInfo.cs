110b299669ea9a1b6a20fc9b5ebd11a7562e574f
Christopher Rhodes <chrhodes@cox.net>
2023-09-13 13:02:27 -0700

    Trying to recover from failing to pull on VNCDev

--- VNCLogViewer/Properties/AssemblyInfo.cs
+++ VNCLogViewer/Properties/AssemblyInfo.cs
@@ -86,4 +86,4 @@ using System.Windows;
 //  1.0.0.0   yyyy/mm/dd    Developer
 //      Initial Version
 
-[assembly: AssemblyInformationalVersion("2023.09.11A")]
+[assembly: AssemblyInformationalVersion("2023.09.13A")]
